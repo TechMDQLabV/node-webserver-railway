48c77ff347d2a4b5d00aa44061af4c482a703264
Sergio Garguir <sergiogarguir@gmail.com>
2022-12-18 11:43:47 -0300

    first commit

---
+++ app-old.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer( (req, res ) => {
+
+    res.write('Hello World...');
+
+    res.end();
+})
+.listen( 8080 );
+
+console.log('Escuchando el puerto 8080');
